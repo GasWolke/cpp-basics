991b5c194295fd8e9c5a5bdd31030f0cfdc05e73
Gaser <sanya-belekov@mail.ru>
2018-10-13 14:11:58 +0300

    first commit

---
+++ 1-linear-program/main.cpp
@@ -0,0 +1,19 @@
+#define _USE_MATH_DEFINES 
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	int a;
+	double z1, z2;
+	setlocale(0, "");
+	cout << "Ведите угол: " << endl;
+	cin >> a;
+	z1 = cos(a) + sin(a) + cos((3 * a)) + sin((3 * a));
+	z2 = 2 * sqrt(2)*cos(a)*sin(M_PI_4 + 2*a );
+	cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;
+	return 0;
+}
